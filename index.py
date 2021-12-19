7361ef834ffe20a6f9ab96db9edeae1bb3a10209
Natanael Bezerra de Oliveira <natanbezerr63@gmail.com>
2021-12-19 14:08:28 -0400

    Finalizado contador de paginas

--- index.py
+++ index.py
@@ -1,20 +1,67 @@
-from sys import platform
-from easygui import multenterbox
+# -*- coding: utf-8 -*-
+
+import sys
+import os
 from easygui import diropenbox
-from easygui import fileopenbox
 from PyPDF2 import PdfFileReader
+from tkinter import *
+from  tkinter import ttk
 
-pdfFile = fileopenbox()
-document = PdfFileReader(pdfFile)
-pages = document.getNumPages()
-print(pages)
-# msg = "Adicione suas credenciais do LUCCA"
-# title = "NBO2001"
-# fieldNames = ["Usuario","Senha"]
-# fieldValues = []  # we start with blanks for the values
-# fieldValues = multenterbox(msg,title, fieldNames)
+try:
+    
+    if((sys.platform).upper() == "LINUX"):
+        folders_separator = "/"
+    else:
+        folders_separator = "\\"
+    
+    pdfs = diropenbox()
 
-# file = diropenbox()
-# print(file)
+    totPages = 0
 
-# print(fieldValues)
+    view = Tk()
+    view.title("NBO2001 - Page Count")
+    view.geometry('400x400')
+
+    my_informations_frame = Frame(view)
+    my_informations_frame.pack()
+
+    my_button = ttk.Button(my_informations_frame, text="Sair", command=view.destroy)
+
+
+    my_informations = ttk.Treeview(my_informations_frame)
+    my_informations['columns'] = ('file_name', 'pages_value')
+
+    my_informations.column("#0", width=0,  stretch=NO)
+    my_informations.column("file_name", anchor=W, width=300)
+    my_informations.column("pages_value", anchor=CENTER, width=80)
+
+    my_informations.heading("#0",text="",anchor=CENTER)
+    my_informations.heading('file_name', text="Nome", anchor=W)
+    my_informations.heading('pages_value', text="Total", anchor=CENTER)
+
+
+    idd_value = 0
+    for path, subPath, files in os.walk(pdfs):
+        
+        for file in files:
+            fileOny = file.split(".")
+            if len(fileOny) == 2 and (fileOny[1]).upper() == "PDF":
+                finalPath = (f'{path}{folders_separator}')
+
+                finalPath = (f'{finalPath}{file}')
+                document = PdfFileReader(finalPath)
+                pages = document.getNumPages()
+                totPages = totPages + pages
+                my_informations.insert(parent='', index='end', iid=idd_value, text='',
+                values=(file, pages))
+                idd_value = idd_value + 1
+
+    myLabel = ttk.Label(my_informations_frame, text=(f'Total de paginas = {totPages}'))
+
+    my_informations.pack()
+    myLabel.pack()
+    my_button.pack()
+
+    view.mainloop()
+except:
+    sys.exit()
